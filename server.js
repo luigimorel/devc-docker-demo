c71ccd0bec8aafca610ab3599fbe175968352d42
Luigi Morel <morelmiles@gmail.com>
2021-09-04 00:36:23 +0300

    Project structure

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const path = require("path");
+require("dotenv").config();
+
+const app = express();
+app.use("/static", express.static(path.join(__dirname, "/public")));
+app.use(express.static(path.join(__dirname, "/public/index.html")));
+
+app.listen(process.env.PORT, () => {
+    console.log("Server is now running.");
+});
