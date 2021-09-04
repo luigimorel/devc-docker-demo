e15364754709d45d2cb220bb5a8083c7919968fe
Luigi Morel <morelmiles@gmail.com>
2021-09-04 18:54:58 +0300

    Fix pages not displaying

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const path = require("path");
 require("dotenv").config();
 
 const app = express();
-app.use("/static", express.static(path.join(__dirname, "/public")));
+app.use(express.static(path.join(__dirname, "/public")));
 app.use(express.static(path.join(__dirname, "/public/index.html")));
+app.use(express.static(path.join(__dirname, "/public/styles/main.css")));
+
 
 app.listen(process.env.PORT, () => {
     console.log("Server is now running.");
